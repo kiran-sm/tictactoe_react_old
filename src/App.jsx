7f1dfec5a57284cb223e7cae95aa70c94d2eb21b
Kiran <smkiran.09@gmail.com>
2023-11-15 15:16:12 +0530

    project setup done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,20 @@ function App() {
   return (
     <>
       <main>
-        <div id="game-container">no updates</div>
+        <div id="game-container">
+          <ol id="players">
+            <li>
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </li>
+            <li>
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">0</span>
+            </li>
+          </ol>
+          GAME BOARD
+        </div>
+        LOG
       </main>
     </>
   );
