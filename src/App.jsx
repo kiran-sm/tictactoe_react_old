72e519aa856f46556ad2eb24b6f865d4ce67aaa3
Kiran <smkiran.09@gmail.com>
2023-11-26 12:12:12 +0530

    bug accrs

--- src/App.jsx
+++ src/App.jsx
@@ -23,29 +23,24 @@ function App() {
     });
   }
   return (
-    <Fragment>
-      <main>
-        <div id="game-container">
-          <ol id="players" className="highlight-player">
-            <Player
-              intialName="Player 1"
-              symbol="X"
-              isActive={activePlayer === "X"}
-            />
-            <Player
-              intialName="Player 2"
-              symbol="0"
-              isActive={activePlayer === "O"}
-            />
-          </ol>
-          <GameBoard
-            onSelectSquare={handleSelectSquare}
-            activePlayerSymbol={activePlayer}
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            intialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
           />
-        </div>
-        <Log />
-      </main>
-    </Fragment>
+          <Player
+            intialName="Player 2"
+            symbol="0"
+            isActive={activePlayer === "O"}
+          />
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+      </div>
+      <Log />
+    </main>
   );
 }
 
